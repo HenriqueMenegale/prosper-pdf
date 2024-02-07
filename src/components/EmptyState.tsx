4f401b8eb5f18982e603f8f4d0528fad5b3ce252
Henrique Menegale <hello@henriquemenegale.com>
2024-02-07 10:09:41 -0300

    Add icons

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,13 @@
+function EmptyState(){
+    return (
+        <div className="text-center w-6/12 self-center justify-self-center mt-20">
+            <img src="" alt="" />
+            <h2 className="text-3xl font-semibold">How to use</h2>
+            <p className="mt-2">
+                Type your question in the field bellow, the AI assistant will answer it based on the information present in the document.
+            </p>
+        </div>
+    )
+}
+
+export default EmptyState;
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Message from './Message';
 import IMessage from '../../Types/Message';
+import EmptyState from '../EmptyState';
 
 type Props = {
     messages: IMessage[] | undefined;
@@ -9,12 +10,14 @@ type Props = {
 function Messages({ messages }: Props){
     return (
         <div className="flex flex-col w-full p-4">
-            {messages && (
+            {messages?.length ? (
                 <>
                     { messages.map((message: IMessage, index) => (
                         <Message message={message} key={ index } />
                     ))}
                 </>
+            ): (
+                <EmptyState />
             )}
         </div>        
     )
