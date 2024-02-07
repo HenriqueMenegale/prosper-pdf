cbfb54f548387acbdf22be2908558a203ed42adc
Henrique Menegale <hello@henriquemenegale.com>
2024-02-07 10:22:01 -0300

    update icon props

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,7 +1,8 @@
+import InfoIcon from "./icons/InfoIcon";
 function EmptyState(){
     return (
         <div className="text-center w-6/12 self-center justify-self-center mt-20">
-            <img src="" alt="" />
+            <InfoIcon />
             <h2 className="text-3xl font-semibold">How to use</h2>
             <p className="mt-2">
                 Type your question in the field bellow, the AI assistant will answer it based on the information present in the document.
