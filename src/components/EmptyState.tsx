025f658401245304dc61cdbb8e4ddb44c2076177
Henrique Menegale <hello@henriquemenegale.com>
2024-02-07 10:33:42 -0300

    Update empty state

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,9 +1,9 @@
 import InfoIcon from "./icons/InfoIcon";
 function EmptyState(){
     return (
-        <div className="text-center w-6/12 self-center justify-self-center mt-20">
-            <InfoIcon />
-            <h2 className="text-3xl font-semibold">How to use</h2>
+        <div className="text-center w-6/12 mt-20 self-center items-center justify-center flex flex-col">
+            <InfoIcon size={40} />
+            <h2 className="text-3xl font-semibold mt-2">How to use</h2>
             <p className="mt-2">
                 Type your question in the field bellow, the AI assistant will answer it based on the information present in the document.
             </p>
